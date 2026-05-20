712241821df5d97ae57cda2576aef83bec5fc078
Cristopher Gabriel Vera Quispe <114591444+CristopherVQ1024@users.noreply.github.com>
2026-05-20 18:21:40 -0500

    Replica DB

--- meditek-back/src/prisma/prisma.service.ts
+++ meditek-back/src/prisma/prisma.service.ts
@@ -2,11 +2,29 @@ import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit {
+export class PrismaService extends PrismaClient implements OnModuleInit {
+
+  private replica: PrismaClient;
+
+  constructor() {
+    super();
+    this.replica = new PrismaClient({
+      datasources: {
+        db: { url: process.env.DATABASE_REPLICA_URL }
+      }
+    });
+  }
 
   async onModuleInit() {
     await this.$connect();
+    await this.replica.$connect();
+  }
+
+  async replicateCreate(model: string, args: any) {
+    try {
+      await (this.replica as any)[model].create(args);
+    } catch (e) {
+      console.error('Error replicando:', e);
+    }
   }
 }
